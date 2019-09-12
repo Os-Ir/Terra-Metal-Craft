ee59536422bd015c4db6c4d64e5f04142013f61d
Os-Ir <AtomAl@163.com>
2019-09-12 16:24:57 +0800

    为燃烧中的锻造炉添加发光和粒子效果

--- src/main/java/com/osir/tmc/block/BlockOriginalForge.java
+++ src/main/java/com/osir/tmc/block/BlockOriginalForge.java
@@ -1,5 +1,7 @@
 package com.osir.tmc.block;
 
+import java.util.Random;
+
 import com.osir.tmc.CreativeTabList;
 import com.osir.tmc.Main;
 import com.osir.tmc.te.TEOriginalForge;
@@ -12,12 +14,16 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockOriginalForge extends TEBlock {
@@ -34,6 +40,51 @@ public class BlockOriginalForge extends TEBlock {
 		this.setCreativeTab(CreativeTabList.tabEquipment);
 		this.setDefaultState(
 				this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(BURN, false));
+	}
+
+	@Override
+	public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
+		if (state.getValue(BURN)) {
+			return 15;
+		}
+		return 0;
+	}
+
+	@Override
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+		if (!state.getValue(BURN)) {
+			return;
+		}
+		EnumFacing facing = state.getValue(FACING);
+		double tx = pos.getX() + 0.5;
+		double ty = pos.getY() + (rand.nextDouble() * 5 + 1) / 16;
+		double tz = pos.getZ() + 0.5;
+		double wdt = rand.nextDouble() * 0.625 - 0.3125;
+		double dlt = 0.52;
+		if (rand.nextDouble() < 0.1) {
+			world.playSound(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE,
+					SoundCategory.BLOCKS, 1, 1, false);
+		}
+		switch (facing) {
+		case WEST:
+			world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, tx - dlt, ty, tz + wdt, 0, 0, 0);
+			world.spawnParticle(EnumParticleTypes.FLAME, tx - dlt, ty, tz + wdt, 0, 0, 0);
+			break;
+		case EAST:
+			world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, tx + dlt, ty, tz + wdt, 0, 0, 0);
+			world.spawnParticle(EnumParticleTypes.FLAME, tx + dlt, ty, tz + wdt, 0, 0, 0);
+			break;
+		case NORTH:
+			world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, tx + wdt, ty, tz - dlt, 0, 0, 0);
+			world.spawnParticle(EnumParticleTypes.FLAME, tx + wdt, ty, tz - dlt, 0, 0, 0);
+			break;
+		case SOUTH:
+			world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, tx + wdt, ty, tz + dlt, 0, 0, 0);
+			world.spawnParticle(EnumParticleTypes.FLAME, tx + wdt, ty, tz + dlt, 0, 0, 0);
+			break;
+		default:
+			break;
+		}
 	}
 
 	@Override
